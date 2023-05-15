0d4a7b91f7c4bcfa24e3afe7bb044ae6c415004b
WhoGetHerHeart <56502495+WhoGetHerHeart@users.noreply.github.com>
2023-05-15 15:46:56 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+baseUrl = "https://m.baidu.com/sugrec?pre=1&p=3&ie=utf-8&json=1&prod=wise&from=wise_web&callback=jsonp3&wd="
+sou_suo = './词包/搜索词包.json'
+fou_ci = './词包/排除词包.json'
+ua = './词包/userAgent.json'
+times = 50  # 循环次数
+stop_time = 0.35  # 请求间隔时间（爬虫保护机制）
